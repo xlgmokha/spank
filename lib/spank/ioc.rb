5eca1c8e5def4148ef185acf12f36742ad9cc116
mo khan <mo@mokhan.ca>
2013-12-20 16:51:41 -0700

    include meaningful error message.

--- lib/spank/ioc.rb
+++ lib/spank/ioc.rb
@@ -6,12 +6,18 @@ module Spank
       end
 
       def resolve(symbol)
-        raise ContainerError.new unless class_variable_defined?(:@@container)
+        raise create_error unless class_variable_defined?(:@@container)
         @@container.resolve(symbol)
       end
 
       def unbind
         remove_class_variable(:@@container) if class_variable_defined?(:@@container)
+      end
+
+      private
+
+      def create_error
+        ContainerError.new("Spank::IOC.bind_to(container) has not been called.")
       end
     end
   end
